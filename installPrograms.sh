9cf9eb95c02abff19b2f92569fd735cab0439b6e
Eynz6002 <enzoytalosilvapires2006@gmail.com>
2026-04-20 03:19:38 -0300

    Correções do shellcheck

--- installPrograms.sh
+++ installPrograms.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
+set -e
 
 echo "Atualizando o sistema..."
 sudo dnf update -y
 
 echo "Configurando RPMFusion (necessário para Steam e drivers)..."
-sudo dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+sudo dnf install -y \
+"https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm" \
+"https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm"
 
 echo "Adicionando repositório do VSCode..."
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
@@ -18,14 +21,18 @@ echo "Adicionando repositório oficial do Docker CE..."
 sudo curl -fsSLo /etc/yum.repos.d/docker-ce.repo https://download.docker.com/linux/fedora/docker-ce.repo
 
 echo "Adicionando repositório oficial do Unity Hub..."
-sudo sh -c 'echo -e "[unityhub]\nname=Unity Hub\nbaseurl=https://hub.unity3d.com/linux/repos/rpm/stable\nenabled=1\ngpgcheck=1\ngpgkey=https://hub.unity3d.com/linux/repos/rpm/stable/repodata/repomd.xml.key\nrepo_gpgcheck=1" > /etc/yum.repos.d/unityhub.repo'
+sudo sh -c 'echo -e "[unityhub]\nname=Unity Hub\nbaseurl=https://hub.unity3d.com/linux/repos/rpm/stable\nenabled=1\ngpgcheck=1\ngpgkey=https://hub.unity3d.com/linux/repos/rpm/stable/repodata/repomd.xml.key" > /etc/yum.repos.d/unityhub.repo'
 
 echo "Instalando pacotes nativos (RPM)..."
-sudo dnf install -y firefox steam code docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin qbittorrent scrcpy lutris brave-browser unityhub
+sudo dnf install -y firefox steam code docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin qbittorrent lutris brave-browser unityhub
+
+echo "Instalando scrcpy..."
+sudo dnf copr enable zeno/scrcpy -y
+sudo dnf install -y scrcpy
 
 echo "Configurando permissões do Docker..."
 sudo systemctl enable --now docker
-sudo usermod -aG docker $USER
+sudo usermod -aG docker "$USER"
 
 echo "========================================="
 echo "Instalação concluída com sucesso!"
